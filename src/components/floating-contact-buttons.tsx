de3a1c1bc7e15d4cea40dd0dbf189865e188d0cf
NAVEEN G <naveen.contactme1@gmail.com>
2025-12-14 15:00:14 +0000

    small mistake campusbuildsolutions@gmail.com  isthe mail

--- src/components/floating-contact-buttons.tsx
+++ src/components/floating-contact-buttons.tsx
@@ -23,7 +23,7 @@ const socialLinks = [
   {
     name: 'Gmail',
     icon: Mail,
-    href: 'https://mail.google.com/mail/?view=cm&fs=1&to=campusbuildsolution@gmail.com',
+    href: 'https://mail.google.com/mail/?view=cm&fs=1&to=campusbuildsolutions@gmail.com',
     bgColor: 'bg-red-500',
   },
   {
